f56bc017b67f5bc1f40b7cee369946bc718e6402
kay Wiberg <kaywib@hotmail.com>
2019-03-05 19:36:35 +0100

    Starting to build

--- StatusCodeResponseService/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace StatusCodeResponseService
-{
-    public class Class1
-    {
-    }
-}
---
+++ StatusCodeResponseService/ResponseService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace StatusCodeResponseService
+{
+    public class ResponseService : ControllerBase
+    {
+        public T GetResponse<T>(int statusCode)
+        {
+
+            switch (statusCode)
+            {
+                case 400:
+                    //
+                    break;
+                case 401:
+                    //
+                    break;
+                case 403:
+                    //
+                    break;
+                case 422:
+                    //
+                    break;
+
+                default:
+                    //
+                    break;
+            }
+
+            return;
+        }
+    }
+}
