2a97c42b2395d1bb82bd22c00b74fb6cd23630f9
kay Wiberg <kaywib@hotmail.com>
2019-03-06 10:17:01 +0100

    Service is working

---
+++ StatusCodeResponseService/Helpers/ResponseServiceConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatusCodeResponseService.Helpers
+{
+   public class ResponseServiceConstants
+    {
+        public const string Code = "Code";
+        public const string Description = "Description";
+
+        public const string DefaultCode = "unknow_error_occured";
+        public const string DefaultDescription = "An unknow error has occured, Please refresh the page";
+    }
+}
--- StatusCodeResponseService/ResponseService.cs
+++ StatusCodeResponseService/ResponseService.cs
@@ -1,34 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using APIErrorHandling;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using StatusCodeResponseService.Helpers;
 using System;
+using System.Reflection;
 
 namespace StatusCodeResponseService
 {
-    public class ResponseService : ControllerBase
+    public static class ResponseService
     {
-        public T GetResponse<T>(int statusCode)
+        public static R GetResponse<R, T>(T _object, ModelStateDictionary modelState)
         {
+            Type objectType = _object.GetType();
+            Type R_instanceType = typeof(R);
 
-            switch (statusCode)
+            object[] args = null;
+
+            bool hasCodeProp = _object.HasProperty(ResponseServiceConstants.Code);
+            bool hasDescriptionProp = _object.HasProperty(ResponseServiceConstants.Description);
+
+            if (hasCodeProp == true && hasDescriptionProp == true)
             {
-                case 400:
-                    //
-                    break;
-                case 401:
-                    //
-                    break;
-                case 403:
-                    //
-                    break;
-                case 422:
-                    //
-                    break;
 
-                default:
-                    //
-                    break;
+                PropertyInfo codeInfo = objectType.GetProperty(ResponseServiceConstants.Code);
+                PropertyInfo descriptionInfo = objectType.GetProperty(ResponseServiceConstants.Description);
+
+                string codeValue = codeInfo.GetValue(_object).ToString();
+                string descriptionValue = descriptionInfo.GetValue(_object).ToString();
+
+                args = new object[]
+                {
+                    Errors.AddErrorToModelState(codeValue, descriptionValue, modelState)
+                };
+            }
+            else
+            {
+                args = new object[]
+               {
+                    Errors.AddErrorToModelState(
+                        ResponseServiceConstants.DefaultCode,
+                        ResponseServiceConstants.DefaultDescription,
+                        modelState
+                        )
+               };
             }
 
-            return;
+            R R_objectInstance = (R)Activator.CreateInstance(R_instanceType, args);
+
+            return R_objectInstance;
+        }
+
+        //Helper
+        private static bool HasProperty(this object obj, string propertyName)
+        {
+            return obj.GetType().GetProperty(propertyName) != null;
         }
     }
 }
